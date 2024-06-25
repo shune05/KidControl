6e0e9842dcc9988a22b4f4b5f07dfe56fe1342ce
tonydinhhuy <146796818+tonydinhhuy@users.noreply.github.com>
2024-06-25 10:50:58 +0700

    Add files via upload

--- KidControl/KidControl/views.py
+++ KidControl/KidControl/views.py
@@ -3,7 +3,7 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template,request,redirect,jsonify, url_for,json
 from KidControl import app
 
 @app.route('/')
@@ -31,6 +31,33 @@ def about():
     """Renders the about page."""
     return render_template(
         'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+@app.route('/login')
+def login():
+    """Renders the about page."""
+    return render_template(
+        'login.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+@app.route('/checklogin', methods=['POST','GET'])
+def checklogin():
+    username = request.form["username"]
+    password = request.form["password"]
+    if username == "abc" and password == "123" and request.method=="POST":
+        return redirect(url_for("viewreport"))
+    else: return redirect(url_for("home"))
+
+
+@app.route('/viewreport')
+def viewreport():
+    """Renders the about page."""
+    return render_template(
+        'viewreport.html',
         title='About',
         year=datetime.now().year,
         message='Your application description page.'
